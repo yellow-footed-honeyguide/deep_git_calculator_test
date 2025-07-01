edaa7b34d6ced4e5a7d145906476a5fcc398342b
Sergey Venetskiy <s.venetsky@gmail.com>
2025-07-01 19:59:29 +0300

    feat: add another dividing

--- calculator_code.py
+++ calculator_code.py
@@ -35,6 +35,8 @@ def main():
     print(f"10 - 5 = {calc.subtract(10, 5)}")
     print(f"10 * 5 = {calc.multiply(10, 5)}")
     print(f"10 / 5 = {calc.divide(10, 5)}")
+    print(f"10 / 5 = {calc.divide(10, 5)}")
+
 
 if __name__ == "__main__":
     main()
